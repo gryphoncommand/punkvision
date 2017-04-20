143a74e94cca3502cd41eddc31300c2a62bddfb5
CadeBrown <cade@lnstempunks.org>
2017-04-19 20:49:54 -0400

    Updated configs

--- autostart.sh
+++ autostart.sh
@@ -6,14 +6,8 @@
 export PUNKVISION=/home/pi/punkvision
 
 # Camera 0, and 1
-/usr/bin/env python ${PUNKVISION}/src/punk.py --source /dev/video0 --stream 5802 --config ${PUNKVISION}/configs/test.conf &
+/usr/bin/env python ${PUNKVISION}/src/punk.py --source /dev/video0 --stream 5802 --config ${PUNKVISION}/configs/Houston-gearpeg.conf &
 #/usr/bin/env python ${PUNKVISION}/src/punk.py --source /dev/video1 --stream 5803 --config ${PUNKVISION}/configs/SMR-gearpeg.conf &
-
-#Use this for serial connection to arduino UNO
-#/usr/bin/env python ${PUNKVISION}/leds/interface.py --serial /dev/ttyACM0 &
-
-#Use this for ethernet connection to arduino Ethernet
-#/usr/bin/env python ${PUNKVISION}/leds/interface.py &
 
 #Reads from lidar
 #/usr/bin/env python ${PUNKVISION}/src/lidar.py --publish &
